ab6706634452ef49423f42fb3a825063cf463f85
Human Resources <humanresources@Humans-MacBook-Pro.local>
2023-08-23 15:38:35 -0700

    base variables used throughout script

--- Lincoln.py
+++ Lincoln.py
@@ -4,3 +4,29 @@ import datetime as dt
 import glob
 import xlsxwriter
 import sys
+
+
+## Used throughout script
+na_vals = ["NA", "Missing"] ###############
+benefitCode = {             ###############
+    "ADDL1": None,
+    "ADDL2": None,
+    "ADDL3": None,
+    "ADDL4": None,
+    "ADDLE": None,
+    "LIFLC": None,
+    "LIFLE": None,
+    "LIFLS": None,
+    "LIFL1": None,
+    "LIFL2": None,
+    "LIFL3": None,
+    "LIFL4": None,
+    "LTDL": None,
+}
+# glEEcode = "2404"      // not sure where to find these, are they relevant for lincoln???
+# glERCobra = "1405"
+glERcode = "7102"        ##############
+glCompany = "2003"       ##############
+benefit = 'Lincoln'      ##############
+monthOVRD = False
+monthYearOVRD = '07-2023'
